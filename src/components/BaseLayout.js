fcdd49365e9599e2799fb9d1e8b14c49e3a98ea9
Jimmy <minhthusdb@gmail.com>
2022-07-17 09:58:44 +0700

    okeee

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -4,7 +4,7 @@ import Style1 from "./home/Home.module.scss";
 import Navbar from "./Navbar";
 import Home from "./home/Home";
 import About from "./about/About";
-//import Portfolio from "./portfolio/Portfolio";
+import Portfolio from "./portfolio/Portfolio";
 import { Route, Routes } from "react-router-dom";
 import { Box, Grid, Typography } from "@mui/material";
 
@@ -37,7 +37,7 @@ export default function BaseLayout() {
           <Routes>
             <Route exact path={"/"} element={<Home />} />
             <Route exact path={"/about"} element={<About />} />
-            <Route exact path={"/portfolio"} />
+            <Route exact path={"/portfolio"} element={<Portfolio />} />
           </Routes>
         </Grid>
         <Grid item>
