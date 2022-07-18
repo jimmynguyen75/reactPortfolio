499c7b2df73f660a4d9f939a1f0774f53406cecf
Jimmy <minhthusdb@gmail.com>
2022-07-18 12:14:48 +0700

    ready to up

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,5 +1,5 @@
 import self from "../img/self.png";
-import mock1 from "../img/mock1.png";
+import mock1 from "../img/mock5.png";
 import mock2 from "../img/mock2.png";
 import mock3 from "../img/mock3.png";
 import mock4 from "../img/mock4.png";
@@ -110,32 +110,32 @@ export const info = {
     // This is where your portfolio projects will be detailed
     {
       title: "Project 1",
-      live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-      source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+      live: "https://www.carworld.tokyo/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+      source: "https://github.com/", // this should be a link to the **repository** of the project, where the code is hosted.
       image: mock1,
     },
     {
       title: "Project 2",
-      live: "https://paytonpierce.dev",
-      source: "https://github.com/paytonjewell",
+      live: "https://www.carworld.tokyo/",
+      source: "https://github.com/",
       image: mock2,
     },
     {
       title: "Project 3",
-      live: "https://paytonpierce.dev",
-      source: "https://github.com/paytonjewell",
+      live: "https://www.carworld.tokyo/",
+      source: "https://github.com/",
       image: mock3,
     },
     {
       title: "Project 4",
-      live: "https://paytonpierce.dev",
-      source: "https://github.com/paytonjewell",
+      live: "https://www.carworld.tokyo/",
+      source: "https://github.com/",
       image: mock4,
     },
     {
       title: "Project 5",
-      live: "https://paytonpierce.dev",
-      source: "https://github.com/paytonjewell",
+      live: "https://www.carworld.tokyo/",
+      source: "https://github.com/",
       image: mock5,
     },
   ],
